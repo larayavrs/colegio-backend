e63abed629ba39e2ad2175060c79b868123b2096
Leo Araya <larayavrs@gmail.com>
2025-07-04 18:22:15 -0400

    feat: add routes for courses and scores management

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ router.get('/', (_, res) =>
   res.send(`Welcome to ${config.global.app_name} API!`),
 );
 router.use('/users', require('./users.route'));
+router.use('/courses', require('./courses.route'));
+router.use('/scores', require('./scores.route'));
 
 /* exporting the router */
 module.exports = router;
