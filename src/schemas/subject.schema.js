49f14c883e558ef340bd0eb49fba1a631c3c8551
Leo Araya <larayavrs@gmail.com>
2025-07-03 20:25:40 -0400

    feat: enforce required field for teachers in subject schema

--- src/schemas/subject.schema.js
+++ src/schemas/subject.schema.js
@@ -2,7 +2,13 @@ const { Schema, model } = require('mongoose');
 
 const subjectSchema = new Schema({
   name: { type: String, required: true, unique: true },
-  teachers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  teachers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  ], // un profesor o mas pueden impartir la materia
 });
 
 module.exports = model('Subject', subjectSchema);
