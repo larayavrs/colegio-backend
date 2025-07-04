17efda0dc3ac738cde3408aa467dbdfaee301a0a
Leo Araya <larayavrs@gmail.com>
2025-07-03 20:22:15 -0400

    feat: refactoring this schema

--- src/schemas/course.schema.js
+++ src/schemas/course.schema.js
@@ -1,22 +1,21 @@
 const { Schema, model } = require('mongoose');
 
 const courseSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  level: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   stage: {
     type: String,
     enum: ['basica', 'media'],
     required: true,
   },
-  capacity: {
-    type: Number,
-    required: true,
-  },
+  capacity: { type: Number, default: 0 },
   supervisorId: {
-    // profesor jefe
     type: Schema.Types.ObjectId,
+    ref: 'User', // profesor jefe
     required: true,
-    ref: 'User',
   },
 });
 
